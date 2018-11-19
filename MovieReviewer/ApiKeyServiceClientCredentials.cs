f554633a632b94a783c1da6824a09e7341382565
Mogens Bjerregaard <tra-mobj@kamstrup.com>
2018-11-19 13:03:23 +0100

    start

---
+++ MovieReviewer/ApiKeyServiceClientCredentials.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Rest;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MovieReviewer
+{
+    public class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private const string SubscriptionKey = "";
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            request.Headers.Add("Ocp-Apim-Subscription-Key", SubscriptionKey);
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+
+}
---
+++ MovieReviewer/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+
+namespace MovieReviewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ITextAnalyticsClient _client = new TextAnalyticsClient(new ApiKeyServiceClientCredentials())
+        {
+            Endpoint = "https://westus.api.cognitive.microsoft.com"
+        };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var inputString = InputReview.Text;
+            InputReview.Text = "";
+            Review.Content = "";
+            Review.Content = inputString;
+            var result = _client.SentimentAsync(
+                new MultiLanguageBatchInput(
+                    new List<MultiLanguageInput>()
+                    {
+                        new MultiLanguageInput("en", "0", inputString)
+                    })).Result;
+            var score = result.Documents[0].Score;
+            Score.Value = 0;
+            Score.Value = (double) (score ?? 0);
+
+        }
+    }
+}
